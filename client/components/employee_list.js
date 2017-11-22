23bf4cfdba803a20922aacfba9dc72f3f51fa414
Hobinx <binservmail@gmail.com>
2017-11-22 22:12:00 +0800

    replace deprecated `createContainer` with `withTracker`

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { createContainer } from 'meteor/react-meteor-data';
+import { withTracker } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 
-const EmployeeList = () => {
+const EmployeeList = (props) => {
+  console.log(props.employees);
   return (
     <div>
       <div className="employee-list">
@@ -12,10 +13,10 @@ const EmployeeList = () => {
   );
 };
 
-export default createContainer(() => {
+export default withTracker(props => {
   // set up subscription
-  Meteor.subscribe('employees');
+  Meteor.subscribe('employees', props.employees);
 
   // return an object, Whatever we return will be sent to EmployeeList as props
   return { employees: Employees.find({}).fetch() };
-}, EmployeeList);
+})(EmployeeList);
