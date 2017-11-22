889437abffbd7bff0f73277188fc87cff26118a5
Hobinx <binservmail@gmail.com>
2017-11-22 22:32:41 +0800

    implement EmployeeDetail component

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
+import EmployeeDetail from './employee_detail';
 
 const EmployeeList = (props) => {
-  console.log(props.employees);
   return (
     <div>
       <div className="employee-list">
-        Employee List
+        {
+          props.employees.map(employee => {
+            return <EmployeeDetail key={employee._id} employee={employee} />;
+          })
+        }
       </div>
     </div>
   );
