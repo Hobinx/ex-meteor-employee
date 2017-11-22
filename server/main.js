3aa818b8e7a8b90d2bfc45dc2a3fb25e05ee0a2d
Hobinx <binservmail@gmail.com>
2017-11-22 21:33:51 +0800

    generate seed data

--- server/main.js
+++ server/main.js
@@ -1,5 +1,22 @@
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employee';
+import R from 'ramda';
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  const numRecords = Employees.find({}).count();
+
+  console.log(numRecords);
+  if (!numRecords) {
+    R.times(() => {
+      const { name, email, phone } = helpers.createCard();
+
+      Employees.insert({
+        name, email, phone,
+        avatar: image.avatar()
+      });
+    }, 5000);
+  }
 });
